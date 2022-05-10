4220cf18fe192cdc7266a21b205b6e3e272f7933
roomMaker <leehuk0622@gmail.com>
2022-05-10 11:22:51 +0900

    Feat. 랜더러 추가

--- ConsoleGame/ConsoleGame/App.c
+++ ConsoleGame/ConsoleGame/App.c
@@ -5,14 +5,29 @@ bool App_Init()
 	return true;
 }
 
+void processInput()
+{
+
+}
+
+void update()
+{
+
+}
+
+void render()
+{
+	puts("Hello Game");
+}
+
 int32 App_Run()
 {
 	//Game Loop : 게임을 동작시키는 루프, 프레임(Frame) 이라고도 한다.
 	while (true)
 	{
-		processInput();
-		update();
-		render();
+		processInput(); // 입력 처리
+		update(); // 게임 업데이트
+		render(); // 게임 출력
 	}
 
 	return 0;
--- ConsoleGame/ConsoleGame/Framework/App.c
+++ ConsoleGame/ConsoleGame/Framework/App.c
@@ -1,12 +1,48 @@
 #include "App.h"
+#include "Common.h"
+#include "Renderer.h"
 
 bool App_Init()
 {
-	return true;
+    if (false == Renderer_Init())
+    {
+        return false;
+    }
+
+    return true;
+}
+
+void processInput()
+{
+
+}
+
+void update()
+{
+    Renderer_DrawText("Hello Game", sizeof("Hello Game"));
+}
+
+void render()
+{
+    Renderer_Flip();
+}
+
+void cleanup()
+{
+    Renderer_Cleanup();
 }
 
 int32 App_Run()
 {
+    atexit(cleanup);
 
-	return 0;
+    // Game Loop : 게임을 동작시키는 루프. 프레임(Frame)이라고 한다.
+    while (true)
+    {
+        processInput();      // 입력 처리
+        update();         // 게임 업데이트
+        render();         // 게임 출력
+    }
+
+    return 0;
 }
